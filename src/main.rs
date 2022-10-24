108b04481a468303b9bb82aedb4ed08aaf433d32
Jackson Ly <bobjacklee27@gmail.com>
2022-10-25 10:22:56 +1100

    Added project_point Also changed from a 1D vec to a 2D array

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-struct Point {
+#[derive(Debug)]
+struct Point3D {
     x: i32,
     y: i32,
     z: i32,
 }
 
+#[derive(Debug)]
+struct Point2D {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
 struct Edge {
     a: usize,
     b: usize,
@@ -11,25 +19,27 @@ struct Edge {
 
 fn main() {
     // Size as in width or height, will always be in a square ratio
-    const SIZE: usize = 3;
+    const SIZE: usize = 20;
+    const FOCAL_LENGTH: i32 = 3;
 
-    let screen: Vec<u8> = vec!();
+    // TODO: Render loop which resets screen
+    let mut screen: [[u8; SIZE]; SIZE] = [[0; SIZE]; SIZE];
 
     // TODO: Make a vertex and edge table variable
     // This is a the vert_table and edge_table for a cube
-    let vert_table: Vec<Point> = vec!(
-        Point { x: -4, y: 4, z: 4 },
-        Point { x: 4, y: 4, z: 4 },
-        Point { x: 4, y: -4, z: 4 },
-        Point { x: -4, y: -4, z: 4 },
-        Point { x: -4, y: 4, z: -4 },
-        Point { x: 4, y: 4, z: -4 },
-        Point { x: 4, y: -4, z: -4 },
-        Point { x: -4, y: -4, z: -4 },
+    let vert_table: Vec<Point3D> = vec!(
+        Point3D { x: -10, y: 10, z: 10 },
+        Point3D { x: 10, y: 10, z: 10 },
+        Point3D { x: 10, y: -10, z: 10 },
+        Point3D { x: -10, y: -10, z: 10 },
+        Point3D { x: -10, y: 10, z: -10 },
+        Point3D { x: 10, y: 10, z: -10 },
+        Point3D { x: 10, y: -10, z: -10 },
+        Point3D { x: -10, y: -10, z: -10 },
     );
 
     // Assign edges by the index of the verts in the vert_table
-    let edge_table: Vec<usize> = vec!(
+    let edge_table: Vec<Edge> = vec!(
         // Front Face
         Edge { a: 0, b: 1 },
         Edge { a: 1, b: 2 },
@@ -49,8 +59,18 @@ fn main() {
         Edge { a: 3, b: 7 },
     );
 
-    // TODO: Implement modifying and pushing to a vec
     // TODO: Project verticies
+    // NOTE: Might have to move from ints to floats
+    fn project_point( point: &Point3D, focal_length: &i32) -> Point2D {
+        let p_x: i32 = (focal_length * point.x) / (focal_length + point.z);
+        let p_y: i32 = (focal_length * point.y) / (focal_length + point.z);
+
+        let projected_point: Point2D = Point2D { x: p_x, y: p_y };
+
+        return projected_point;
+    }
+
+    // TODO: Implement modifying and pushing to a vec, this is the display
     // TODO: Find equation of a line between two points
     //          Need to find a way to restrict domain?
     // TODO: Implement matrix rotation
@@ -58,12 +78,23 @@ fn main() {
     //          This needs a theta variable as well
     //          Probably watch 3b1b linear algebra series
 
+    let a = project_point(&vert_table[0], &FOCAL_LENGTH);
+    
+    println!("{:?}", a);
+
     // "Render" 1D array as a 2D array
-    for i in 0..SIZE*SIZE {
-        if (i+1) % SIZE != 0 {
-            print!("{:?}", if screen[i] == 1 {"##"} else {"  "});
-        } else {
-            print!("{:?}\n", if screen[i] == 1 {"##"} else {"  "})
+    // for i in 0..SIZE*SIZE {
+    //     if (i+1) % SIZE != 0 {
+    //         print!("{:?}", if screen[i] == 1 {"##"} else {"  "});
+    //     } else {
+    //         print!("{:?}\n", if screen[i] == 1 {"##"} else {"  "})
+    //     }
+    // }
+
+    for row in screen.iter_mut() {
+        for cell in row.iter_mut() {
+            // modify cell by *cell = int;
+            print!("{:?}", cell);
         }
     }
 }
