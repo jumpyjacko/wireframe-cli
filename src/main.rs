ad795b9095fd3a2327b08bd921cff1b1cc67cc99
Jackson Ly <bobjacklee27@gmail.com>
2022-10-25 12:08:12 +1100

    Added line plotting

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ struct Edge {
 
 fn main() {
     // Size as in width or height, will always be in a square ratio
-    const SIZE: usize = 20;
+    const SIZE: usize = 30;
     const FOCAL_LENGTH: i32 = 3;
 
     // TODO: Render loop which resets screen
@@ -58,7 +58,7 @@ fn main() {
         Edge { a: 2, b: 6 },
         Edge { a: 3, b: 7 },
     );
-
+    
     // TODO: Project verticies
     // NOTE: Might have to move from ints to floats
     fn project_point( point: &Point3D, focal_length: &i32) -> Point2D {
@@ -70,16 +70,40 @@ fn main() {
         return projected_point;
     }
 
-    // TODO: Implement modifying and pushing to a vec, this is the display
     // TODO: Find equation of a line between two points
     //          Need to find a way to restrict domain?
+    // TODO: Implement modifying and pushing to a vec, this is the display
+    // Uses Bresenham's Line Algorithm (integer arithmetic)
+    fn plot_line(point1: Point2D, point2: Point2D, mut screen: [[u8; SIZE]; SIZE]) -> [[u8; SIZE]; SIZE] {
+        let dx = point2.x - point1.x;
+        let dy = point2.y - point1.x;
+        let mut d = 2*dy - dx;
+        let mut y = point1.y;
+
+        for x in point1.x..point2.x {
+            screen[y as usize][x as usize] = 1;
+
+            if d > 0 {
+                y = y+1;
+                d = d-2*dx;
+            };
+            d = d+2*dy;
+        };
+        return screen;
+    }
+
+    // Plots the points into the 2D array
+    fn plot(point: Point2D) {
+
+    }
+
     // TODO: Implement matrix rotation
     //          If I'm lazy, do the 2D matrix rotation, else do the full rotation matrix
     //          This needs a theta variable as well
     //          Probably watch 3b1b linear algebra series
 
     let a = project_point(&vert_table[0], &FOCAL_LENGTH);
-    
+
     println!("{:?}", a);
 
     // "Render" 1D array as a 2D array
@@ -91,10 +115,13 @@ fn main() {
     //     }
     // }
 
+    screen = plot_line( Point2D {x: 0, y: 0}, Point2D {x: 3, y: 3}, screen);
+
     for row in screen.iter_mut() {
         for cell in row.iter_mut() {
             // modify cell by *cell = int;
-            print!("{:?}", cell);
+            print!("{:?}", if *cell as u8 == 1 {"##"} else {"  "});
         }
+        print!("\n");
     }
 }
