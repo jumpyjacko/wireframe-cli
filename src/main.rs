adc8d5cbb244b4960fdfa9b2099c9916ff4d7c83
JumpyJacko <bobjacklee27@gmail.com>
2022-10-25 17:50:04 +1100

    First working version

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,23 @@ struct Edge {
 
 fn main() {
     // Size as in width or height, will always be in a square ratio
-    const SIZE: usize = 30;
-    const FOCAL_LENGTH: i32 = 3;
+    const SIZE: usize = 40;
+    const FOCAL_LENGTH: i32 = 40;
 
-    // TODO: Render loop which resets screen
+    // TODO: Render loop which resets screen so I can animate
     let mut screen: [[u8; SIZE]; SIZE] = [[0; SIZE]; SIZE];
 
-    // TODO: Make a vertex and edge table variable
-    // This is a the vert_table and edge_table for a cube
+    // FIXME: Everything here renders upside down lol, probably due to the way I index and then plot
+    // NOTE: This is a the vert_table and edge_table for a cube
     let vert_table: Vec<Point3D> = vec!(
-        Point3D { x: -10, y: 10, z: 10 },
-        Point3D { x: 10, y: 10, z: 10 },
-        Point3D { x: 10, y: -10, z: 10 },
-        Point3D { x: -10, y: -10, z: 10 },
-        Point3D { x: -10, y: 10, z: -10 },
-        Point3D { x: 10, y: 10, z: -10 },
-        Point3D { x: 10, y: -10, z: -10 },
-        Point3D { x: -10, y: -10, z: -10 },
+        Point3D { x: -50, y: 50, z: 50 },
+        Point3D { x: 50, y: 50, z: 50 },
+        Point3D { x: 50, y: -50, z: 50 },
+        Point3D { x: -50, y: -50, z: 50 },
+        Point3D { x: -50, y: 50, z: -50 },
+        Point3D { x: 50, y: 50, z: -50 },
+        Point3D { x: 50, y: -50, z: -50 },
+        Point3D { x: -50, y: -50, z: -50 },
     );
 
     // Assign edges by the index of the verts in the vert_table
@@ -58,69 +58,114 @@ fn main() {
         Edge { a: 2, b: 6 },
         Edge { a: 3, b: 7 },
     );
+
+    // NOTE: This is a the vert_table and edge_table for a square-based pyramid FIXME: This is not working right now
+    // let vert_table: Vec<Point3D> = vec!(
+    //     Point3D { x: -50, y: -50, z: -50 },
+    //     Point3D { x: 50, y: -50, z: -50 },
+    //     Point3D { x: 50, y: -50, z: 50 },
+    //     Point3D { x: -50, y: -50, z: 50 },
+    //     Point3D { x: 0, y: 50, z: 0 },
+    // );
+
+    // // Assign edges by the index of the verts in the vert_table
+    // let edge_table: Vec<Edge> = vec!(
+    //     // Base Face
+    //     Edge { a: 0, b: 1 },
+    //     Edge { a: 1, b: 2 },
+    //     Edge { a: 2, b: 3 },
+    //     Edge { a: 3, b: 0 },
+        
+    //     // Base-to-Point Edges
+    //     Edge { a: 0, b: 4 },
+    //     Edge { a: 1, b: 4 },
+    //     Edge { a: 2, b: 4 },
+    //     Edge { a: 3, b: 4 },
+    // );
+
+    let mut projected_vert_table: Vec<Point2D> = vec!();
     
-    // TODO: Project verticies
-    // NOTE: Might have to move from ints to floats
-    fn project_point( point: &Point3D, focal_length: &i32) -> Point2D {
-        let p_x: i32 = (focal_length * point.x) / (focal_length + point.z);
-        let p_y: i32 = (focal_length * point.y) / (focal_length + point.z);
+    fn project_point(point: &Point3D, focal_length: &i32) -> Point2D {
+        let p_x: i32 = (focal_length * point.x) / (focal_length + point.z + 256);
+        let p_y: i32 = (focal_length * point.y) / (focal_length + point.z + 256);
 
         let projected_point: Point2D = Point2D { x: p_x, y: p_y };
 
+        println!("{:?}", projected_point);
         return projected_point;
     }
 
-    // TODO: Find equation of a line between two points
-    //          Need to find a way to restrict domain?
-    // TODO: Implement modifying and pushing to a vec, this is the display
-    // Uses Bresenham's Line Algorithm (integer arithmetic)
     fn plot_line(point1: Point2D, point2: Point2D, mut screen: [[u8; SIZE]; SIZE]) -> [[u8; SIZE]; SIZE] {
-        let dx = point2.x - point1.x;
-        let dy = point2.y - point1.x;
-        let mut d = 2*dy - dx;
+        let mut dx = point2.x - point1.x;
+        let mut dy = point2.y - point1.y;
+        // let mut d = 2*dy - dx;
+        // let mut y = point1.y;
+
+        let offset = SIZE as i32/2;
+
+        // Bresenham's Line Algorithm (integer arithmetic version)
+        // for x in if point1.x < point2.x {point1.x..point2.x} else {point2.x..point1.x} {
+        //     screen[(y + offset) as usize][(x + offset) as usize] = 1;
+
+        //     if d > 0 {
+        //         y = y+1;
+        //         d = d-2*dx;
+        //     };
+        //     d = d+2*dy;
+        // };
+
+        // Naive Line Algorithm
+        // for x in if point1.x < point2.x {point1.x..point2.x} else {point2.x..point1.x} {
+        //     let y = point1.y + dy * (x - point1.x) / dx;
+        //     screen[(y + offset) as usize][(x + offset) as usize] = 1;
+        // }
+
+        // Digital Differential Analyzer
+        let step;
+        if dx.abs() >= dy.abs() {
+            step = dx.abs();
+        } else {
+            step = dy.abs();
+        }
+        dx = dx / step;
+        dy = dy / step;
+        let mut x = point1.x;
         let mut y = point1.y;
+        let mut i = 1;
+        while i <= step {
+            screen[(y + offset) as usize][(x + offset) as usize] = 1;
+            x = x + dx;
+            y = y + dy;
+            i += 1;
+        }
 
-        for x in point1.x..point2.x {
-            screen[y as usize][x as usize] = 1;
-
-            if d > 0 {
-                y = y+1;
-                d = d-2*dx;
-            };
-            d = d+2*dy;
-        };
         return screen;
     }
 
-    // Plots the points into the 2D array
-    fn plot(point: Point2D) {
-
+    // Iterates through vert_table and projects them
+    for vert in vert_table.iter() {
+        let p_point = project_point(vert, &FOCAL_LENGTH);
+        projected_vert_table.push(p_point);
     }
+
+    for edge in edge_table.iter() {
+        // println!("({}, {}) ({}, {})", projected_vert_table[edge.a].x, projected_vert_table[edge.a].y, projected_vert_table[edge.b].x, projected_vert_table[edge.b].y);
+        screen = plot_line(Point2D { x: projected_vert_table[edge.a].x, y: projected_vert_table[edge.a].y }, Point2D { x: projected_vert_table[edge.b].x, y: projected_vert_table[edge.b].y }, screen);
+    }
+
+    println!("{:?}", projected_vert_table);
+    // println!("{:?}", edge_table);
 
     // TODO: Implement matrix rotation
     //          If I'm lazy, do the 2D matrix rotation, else do the full rotation matrix
     //          This needs a theta variable as well
     //          Probably watch 3b1b linear algebra series
 
-    let a = project_point(&vert_table[0], &FOCAL_LENGTH);
-
-    println!("{:?}", a);
-
     // "Render" 1D array as a 2D array
-    // for i in 0..SIZE*SIZE {
-    //     if (i+1) % SIZE != 0 {
-    //         print!("{:?}", if screen[i] == 1 {"##"} else {"  "});
-    //     } else {
-    //         print!("{:?}\n", if screen[i] == 1 {"##"} else {"  "})
-    //     }
-    // }
-
-    screen = plot_line( Point2D {x: 0, y: 0}, Point2D {x: 3, y: 3}, screen);
-
     for row in screen.iter_mut() {
         for cell in row.iter_mut() {
             // modify cell by *cell = int;
-            print!("{:?}", if *cell as u8 == 1 {"##"} else {"  "});
+            print!("{}", if *cell as u8 == 1 {"##"} else {".."});
         }
         print!("\n");
     }
