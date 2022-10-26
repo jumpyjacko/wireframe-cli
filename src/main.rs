2ea58e2b0f18136b36452b7d168ade2ba5788dd4
Jackson Ly <bobjacklee27@gmail.com>
2022-10-26 11:51:34 +1100

    Added simple rotation (z-axis)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use ndarray::arr1;
+use ndarray::arr2;
+
 #[derive(Debug)]
 struct Point3D {
-    x: i32,
-    y: i32,
-    z: i32,
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
 #[derive(Debug)]
@@ -27,67 +30,68 @@ fn main() {
 
     // FIXME: Everything here renders upside down lol, probably due to the way I index and then plot
     // NOTE: This is a the vert_table and edge_table for a cube
-    // let vert_table: Vec<Point3D> = vec!(
-    //     Point3D { x: -50, y: 50, z: 50 },
-    //     Point3D { x: 50, y: 50, z: 50 },
-    //     Point3D { x: 50, y: -50, z: 50 },
-    //     Point3D { x: -50, y: -50, z: 50 },
-    //     Point3D { x: -50, y: 50, z: -50 },
-    //     Point3D { x: 50, y: 50, z: -50 },
-    //     Point3D { x: 50, y: -50, z: -50 },
-    //     Point3D { x: -50, y: -50, z: -50 },
-    // );
-
-    // // Assign edges by the index of the verts in the vert_table
-    // let edge_table: Vec<Edge> = vec!(
-    //     // Front Face
-    //     Edge { a: 0, b: 1 },
-    //     Edge { a: 1, b: 2 },
-    //     Edge { a: 2, b: 3 },
-    //     Edge { a: 3, b: 0 },
-
-    //     // Back Face
-    //     Edge { a: 4, b: 5 },
-    //     Edge { a: 5, b: 6 },
-    //     Edge { a: 6, b: 7 },
-    //     Edge { a: 7, b: 4 },
-
-    //     // Connecting the Front and Back Face
-    //     Edge { a: 0, b: 4 },
-    //     Edge { a: 1, b: 5 },
-    //     Edge { a: 2, b: 6 },
-    //     Edge { a: 3, b: 7 },
-    // );
-
-    // NOTE: This is a the vert_table and edge_table for a square-based pyramid FIXME: This is not working right now
     let vert_table: Vec<Point3D> = vec!(
-        Point3D { x: -50, y: -50, z: -50 },
-        Point3D { x: 50, y: -50, z: -50 },
-        Point3D { x: 50, y: -50, z: 50 },
-        Point3D { x: -50, y: -50, z: 50 },
-        Point3D { x: 0, y: 50, z: 0 },
+        Point3D { x: -50.0, y: 50.0, z: 50.0 },
+        Point3D { x: 50.0, y: 50.0, z: 50.0 },
+        Point3D { x: 50.0, y: -50.0, z: 50.0 },
+        Point3D { x: -50.0, y: -50.0, z: 50.0 },
+        Point3D { x: -50.0, y: 50.0, z: -50.0 },
+        Point3D { x: 50.0, y: 50.0, z: -50.0 },
+        Point3D { x: 50.0, y: -50.0, z: -50.0 },
+        Point3D { x: -50.0, y: -50.0, z: -50.0 },
     );
 
     // Assign edges by the index of the verts in the vert_table
     let edge_table: Vec<Edge> = vec!(
-        // Base Face
+        // Front Face
         Edge { a: 0, b: 1 },
         Edge { a: 1, b: 2 },
         Edge { a: 2, b: 3 },
         Edge { a: 3, b: 0 },
-        
-        // Base-to-Point Edges
+
+        // Back Face
+        Edge { a: 4, b: 5 },
+        Edge { a: 5, b: 6 },
+        Edge { a: 6, b: 7 },
+        Edge { a: 7, b: 4 },
+
+        // Connecting the Front and Back Face
         Edge { a: 0, b: 4 },
-        Edge { a: 1, b: 4 },
-        Edge { a: 2, b: 4 },
-        Edge { a: 3, b: 4 },
+        Edge { a: 1, b: 5 },
+        Edge { a: 2, b: 6 },
+        Edge { a: 3, b: 7 },
     );
 
+    // NOTE: This is a the vert_table and edge_table for a square-based pyramid FIXME: This is not working right now
+    // let vert_table: Vec<Point3D> = vec!(
+    //     Point3D { x: -50.0, y: -50.0, z: -50.0 },
+    //     Point3D { x: 50.0, y: -50.0, z: -50.0 },
+    //     Point3D { x: 50.0, y: -50.0, z: 50.0 },
+    //     Point3D { x: -50.0, y: -50.0, z: 50.0 },
+    //     Point3D { x: 0.0, y: 50.0, z: 0.0 },
+    // );
+
+    // // Assign edges by the index of the verts in the vert_table
+    // let edge_table: Vec<Edge> = vec!(
+    //     // Base Face
+    //     Edge { a: 0, b: 1 },
+    //     Edge { a: 1, b: 2 },
+    //     Edge { a: 2, b: 3 },
+    //     Edge { a: 3, b: 0 },
+        
+    //     // Base-to-Point Edges
+    //     Edge { a: 0, b: 4 },
+    //     Edge { a: 1, b: 4 },
+    //     Edge { a: 2, b: 4 },
+    //     Edge { a: 3, b: 4 },
+    // );
+
+    let mut rotated_vert_table: Vec<Point3D> = vec!();
     let mut projected_vert_table: Vec<Point2D> = vec!();
     
     fn project_point(point: &Point3D, focal_length: &i32) -> Point2D {
-        let p_x: i32 = (focal_length * point.x) / (focal_length + point.z + 256);
-        let p_y: i32 = (focal_length * point.y) / (focal_length + point.z + 256);
+        let p_x: i32 = ((*focal_length as f32 * point.x) / (*focal_length as f32 + point.z + 256.0)) as i32;
+        let p_y: i32 = ((*focal_length as f32 * point.y) / (*focal_length as f32 + point.z + 256.0)) as i32;
 
         let projected_point: Point2D = Point2D { x: p_x, y: p_y };
 
@@ -123,9 +127,31 @@ fn main() {
         return screen;
     }
 
+    // TODO: Implement matrix rotation
+    //          If I'm lazy, do the 2D matrix rotation, else do the full rotation matrix
+    //          This needs a theta variable as well
+    //          Probably watch 3b1b linear algebra series
+    fn simple_rotate_z(point: &Point3D, theta: f32) -> Point3D {
+        let z = point.z as f32;
+        let xy = arr1(&[point.x as f32, point.y as f32]);
+        let matrix = arr2(&[[theta.cos(), -theta.sin()],
+                            [theta.sin(), theta.cos()]]);
+
+        let rotated_xy = matrix.dot(&xy);
+
+        let rotated_xyz = Point3D {x: rotated_xy[0], y: rotated_xy[1], z: z };
+
+        return rotated_xyz;
+    }
+
 
     // Iterates through vert_table and adds projected points to another vec
     for vert in vert_table.iter() {
+        let r_point = simple_rotate_z(vert, 90.0);
+        rotated_vert_table.push(r_point);
+    }
+
+    for vert in rotated_vert_table.iter() {
         let p_point = project_point(vert, &FOCAL_LENGTH);
         projected_vert_table.push(p_point);
     }
@@ -133,11 +159,6 @@ fn main() {
     for edge in edge_table.iter() {
         screen = plot_line(Point2D { x: projected_vert_table[edge.a].x, y: projected_vert_table[edge.a].y }, Point2D { x: projected_vert_table[edge.b].x, y: projected_vert_table[edge.b].y }, screen);
     }
-
-    // TODO: Implement matrix rotation
-    //          If I'm lazy, do the 2D matrix rotation, else do the full rotation matrix
-    //          This needs a theta variable as well
-    //          Probably watch 3b1b linear algebra series
 
     // "Renders" 2D array from 0 and 1 to '  ' and '##'
     for row in screen.iter_mut() {
