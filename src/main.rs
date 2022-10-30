77e32fcd587dc646f8e2dcf8558cdb230ee05668
JumpyJacko <bobjacklee27@gmail.com>
2022-10-30 16:38:00 +1100

    Added torus/donut Also added a frame-time flag + readout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use ndarray::arr1;
 use ndarray::arr2;
 use clap::{Arg, Command};
-use std::{thread, time::Duration, time::Instant};
+use std::{
+    thread,
+    time::Duration,
+    time::Instant
+};
 
 struct Point3D {
     x: f32,
@@ -34,7 +38,7 @@ fn main() {
             .short('s')
             .long("shape")
             .default_value("cube")
-            .help("Pick what shape you want to see (only the ones below)\n    (cube, pyramid, star_cube)"))
+            .help("Pick what shape you want to see (only the ones below)\n    (cube, pyramid, star_cube, donut)"))
         .arg(Arg::new("fill")
             .short('f')
             .long("fill")
@@ -45,12 +49,18 @@ fn main() {
             .long("line")
             .default_value("#")
             .help("Pick characters to use for the lines\n    (use only one of that character)"))
+        .arg(Arg::new("frame-time")
+            .short('t')
+            .long("frame-time")
+            .default_value("40")
+            .help("Input how long to hold a frame\n    (in millis)"))
         .get_matches();
 
     
     let fill_char: &str = matches.get_one::<String>("fill").unwrap();
     let line_char: &str = matches.get_one::<String>("line").unwrap();
     let shape: &str = matches.get_one::<String>("shape").unwrap();
+    let frame_time = matches.get_one::<String>("frame-time").unwrap().parse::<u64>().unwrap();
 
     let vert_table: Vec<Point3D>;
     let edge_table: Vec<Edge>;
@@ -140,6 +150,130 @@ fn main() {
                 Edge { a: 5, b: 7 },
             );
         },
+        "donut" => {
+            // How I'm getting values, https://www.desmos.com/calculator/gwpwa2315x
+            vert_table = vec!(
+                // Inner Ring
+                Point3D { x: 0.0, y: 0.0, z: 40.0 },
+                Point3D { x: 28.3, y: 0.0, z: 28.3 },
+                Point3D { x: 40.0, y: 0.0, z: 0.0 },
+                Point3D { x: 28.3, y: 0.0, z: -28.3 },
+                Point3D { x: 0.0, y: 0.0, z: -40.0 },
+                Point3D { x: -28.3, y: 0.0, z: -28.3 },
+                Point3D { x: -40.0, y: 0.0, z: 0.0 },
+                Point3D { x: -28.3, y: 0.0, z: 28.3 },
+
+                // Outer Ring
+                Point3D { x: 0.0, y: 0.0, z: 100.0 },
+                Point3D { x: 70.7, y: 0.0, z: 70.7 },
+                Point3D { x: 100.0, y: 0.0, z: 0.0 },
+                Point3D { x: 70.7, y: 0.0, z: -70.7 },
+                Point3D { x: 0.0, y: 0.0, z: -100.0 },
+                Point3D { x: -70.7, y: 0.0, z: -70.7 },
+                Point3D { x: -100.0, y: 0.0, z: 0.0 },
+                Point3D { x: -70.7, y: 0.0, z: 70.7 },
+
+                // Upper Middle Ring
+                Point3D { x: 0.0, y: 35.0, z: 70.0 },
+                Point3D { x: 49.5, y: 35.0, z: 49.5 },
+                Point3D { x: 70.0, y: 35.0, z: 0.0 },
+                Point3D { x: 49.5, y: 35.0, z: -49.5 },
+                Point3D { x: 0.0, y: 35.0, z: -70.0 },
+                Point3D { x: -49.5, y: 35.0, z: -49.5 },
+                Point3D { x: -70.0, y: 35.0, z: 0.0 },
+                Point3D { x: -49.5, y: 35.0, z: 49.5 },
+                // Lower Middle Ring
+                Point3D { x: 0.0, y: -35.0, z: 70.0 },
+                Point3D { x: 49.5, y: -35.0, z: 49.5 },
+                Point3D { x: 70.0, y: -35.0, z: 0.0 },
+                Point3D { x: 49.5, y: -35.0, z: -49.5 },
+                Point3D { x: 0.0, y: -35.0, z: -70.0 },
+                Point3D { x: -49.5, y: -35.0, z: -49.5 },
+                Point3D { x: -70.0, y: -35.0, z: 0.0 },
+                Point3D { x: -49.5, y: -35.0, z: 49.5 },
+            );
+            edge_table = vec!(
+                // Inner Ring
+                Edge { a: 0, b: 1 },
+                Edge { a: 1, b: 2 },
+                Edge { a: 2, b: 3 },
+                Edge { a: 3, b: 4 },
+                Edge { a: 4, b: 5 },
+                Edge { a: 5, b: 6 },
+                Edge { a: 6, b: 7 },
+                Edge { a: 7, b: 0 },
+
+                // Outer Ring
+                Edge { a: 8, b: 9 },
+                Edge { a: 9, b: 10 },
+                Edge { a: 10, b: 11 },
+                Edge { a: 11, b: 12 },
+                Edge { a: 12, b: 13 },
+                Edge { a: 13, b: 14 },
+                Edge { a: 14, b: 15 },
+                Edge { a: 15, b: 8 },
+
+                // Upper Middle Ring
+                Edge { a: 16, b: 17 },
+                Edge { a: 17, b: 18 },
+                Edge { a: 18, b: 19 },
+                Edge { a: 19, b: 20 },
+                Edge { a: 20, b: 21 },
+                Edge { a: 21, b: 22 },
+                Edge { a: 22, b: 23 },
+                Edge { a: 23, b: 16 },
+                // Lower Middle Ring
+                Edge { a: 24, b: 25 },
+                Edge { a: 25, b: 26 },
+                Edge { a: 26, b: 27 },
+                Edge { a: 27, b: 28 },
+                Edge { a: 28, b: 29 },
+                Edge { a: 29, b: 30 },
+                Edge { a: 30, b: 31 },
+                Edge { a: 31, b: 24 },
+
+                // Joining Rings
+                Edge { a: 0, b: 16 },
+                Edge { a: 16, b: 8 },
+                Edge { a: 8, b: 24 },
+                Edge { a: 24, b: 0 },
+
+                Edge { a: 1, b: 17 },
+                Edge { a: 17, b: 9 },
+                Edge { a: 9, b: 25 },
+                Edge { a: 25, b: 1 },
+
+                Edge { a: 2, b: 18 },
+                Edge { a: 18, b: 10 },
+                Edge { a: 10, b: 26 },
+                Edge { a: 26, b: 2 },
+
+                Edge { a: 3, b: 19 },
+                Edge { a: 19, b: 11 },
+                Edge { a: 11, b: 27 },
+                Edge { a: 27, b: 3 },
+
+                Edge { a: 4, b: 20 },
+                Edge { a: 20, b: 12 },
+                Edge { a: 12, b: 28 },
+                Edge { a: 28, b: 4 },
+
+                Edge { a: 5, b: 21 },
+                Edge { a: 21, b: 13 },
+                Edge { a: 13, b: 29 },
+                Edge { a: 29, b: 5 },
+
+                Edge { a: 6, b: 22 },
+                Edge { a: 22, b: 14 },
+                Edge { a: 14, b: 30 },
+                Edge { a: 30, b: 6 },
+
+                Edge { a: 7, b: 23 },
+                Edge { a: 23, b: 15 },
+                Edge { a: 15, b: 31 },
+                Edge { a: 31, b: 7 },
+            );
+        }
         _ => {
             vert_table = vec!(Point3D { x: 1.0, y: 1.0, z: 1.0 });
             edge_table = vec!(Edge { a:0, b:0 });
@@ -156,7 +290,7 @@ fn main() {
         return projected_point;
     }
 
-    // FIXME: Everything renders upsidedown, probably due to the way I index and then plot
+    // FIXME: Everything renders upside down, probably due to the way I index and then plot
     fn plot_line(point1: Point2D, point2: Point2D, mut screen: [[u8; SIZE]; SIZE]) -> [[u8; SIZE]; SIZE] {
         let offset: f32 = SIZE as f32/2.0;
 
@@ -272,10 +406,11 @@ fn main() {
 
         // Stat. readout
         let duration = timer.elapsed().as_micros();
-        println!("frame time: {} μs", duration);
-        println!("     theta: {}", theta);
+        println!("calculation duration: {} μs", duration);
+        println!("     frame hold time: {} ms", frame_time);
+        println!("               theta: {}", theta);
 
         // Determines how long to hold frame
-        thread::sleep(Duration::from_millis(40));
+        thread::sleep(Duration::from_millis(frame_time));
     }
 }
